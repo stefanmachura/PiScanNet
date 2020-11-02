23f983bd4f193df780062d9a9fd295b7d4e6e38c
stefanmachura <stefanmachura@gmail.com>
2020-11-02 18:02:44 +0100

    added known mac listing

--- netscan_xml.py
+++ netscan_xml.py
@@ -1,34 +1,51 @@
+import json
 import os
 import subprocess
 import xml.etree.ElementTree as ET
 
-subprocess.check_output("sudo nmap -sn -oX a.xml  192.168.2.0/24", shell=True)
+DICT = 1
+TEXT = 2
 
 
-response = ET.parse("a.xml")
-
-root = response.getroot()
-
-hosts = [child for child in root if child.tag ==  "host"]
-
-results = []
-
-for tags in hosts:
-	result = {}
-	for tag in tags:
-		if tag.tag == "status":
-			if tag.get("reason") == "localhost-response":
-				result["comment"] = "(It's me)!"
-			else:
-				result["comment"] = ""
-		if tag.tag == "address" and tag.get("addrtype") == "ipv4":
-			result["ip4"] = tag.get("addr")
-		if tag.tag == "address" and tag.get("addrtype") == "mac":
-			result["mac"] = f"{tag.get('addr')} {tag.get('vendor')}"
-	results.append(result)
-
-for result in results:
-	print(result)
+def get_known_macs():
+    try:
+        with open("addrs.json") as jfile:
+            addrs = json.load(jfile)
+            return addrs
+    except FileNotFoundError:
+        return {}
 
 
-os.remove("a.xml")
+def scan(cached=False, format=DICT):
+    known_macs = get_known_macs()
+    if not cached:
+        subprocess.check_output("sudo nmap -sn -oX tmp.xml  192.168.2.0/24", shell=True)
+    response = ET.parse("tmp.xml")
+    hosts = [child for child in response.getroot() if child.tag == "host"]
+    results = []
+    for tags in hosts:
+        result = {}
+        for tag in tags:
+            if tag.tag == "address" and tag.get("addrtype") == "ipv4":
+                result["ip4"] = tag.get("addr")
+            if tag.tag == "address" and tag.get("addrtype") == "mac":
+                mac_addr = tag.get("addr")
+                if mac_addr in known_macs:
+                    result["mac"] = f"{known_macs[mac_addr]}"
+                else:
+                    result["mac"] = f"{mac_addr} {tag.get('vendor')}"
+        results.append(result)
+    if format == DICT:
+        return results
+    if format == TEXT:
+        result_text = ""
+        for result in results:
+            for _, item in result.items():
+                result_text += item + " "
+            result_text += "\n"
+        return result_text
+    if not cached:
+        os.remove("tmp.xml")
+
+
+print(scan(cached=True, format=TEXT))
