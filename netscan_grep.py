48ea2b539676e05494399331cdecce77fb511ed0
stefanmachura <stefanmachura@gmail.com>
2020-11-02 18:03:49 +0100

    file cleanup

--- netscan_grep.py
+++
@@ -1,9 +0,0 @@
-with open("x.xml") as xfile:
-	devices = xfile.readlines()[1:-1] 
-	
-for device in devices:
-	host, status = device.rstrip("\n").split("\t")
-	host = host.split(": ")[1]
-	status = status.split(": ")[1]
-	print(f"device {host} is {status}")
-
